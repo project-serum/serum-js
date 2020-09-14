13e2eb0e751e9cfe649b4a6ff4ba639d25286f03
philippe-ftx <62417741+philippe-ftx@users.noreply.github.com>
2020-09-14 09:30:19 +0200

    Export decode token instructions function (#17)
    Export decode token instructions function

--- src/token-instructions.js
+++ src/token-instructions.js
@@ -61,6 +61,10 @@ function encodeTokenInstructionData(instruction) {
   return b.slice(0, span);
 }
 
+export function decodeTokenInstructionData(instruction) {
+  return LAYOUT.decode(instruction);
+}
+
 export function initializeMint({
   mint,
   decimals,
